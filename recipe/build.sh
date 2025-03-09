d32d847027518acf87e64acc4d9c051af7bb0a40
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-03-09 13:02:26 -0700

    Change `osx-arm64` build command

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,14 @@
 
 set -xe
 
-# If running in Linux or osx-arm64 set the `$HOSTCC` manually
-# to avoid calling the hardcoded `cc` command in the makefile
-if [[ "${target_platform}" == linux-* || "${target_platform}" == osx-arm64 ]]; then
+# If running in Linux, set the `$HOSTCC` manually to
+# avoid calling the hardcoded `cc` command in the makefile
+if [[ "${target_platform}" == linux-* ]]; then
     make HOSTCC="$CC -g -Wall -pedantic -Wcast-qual"
+# If building for `osx-arm64` use `cc` for `HOSTCC` and `$CC` for `CC`.
+# This is because the `osx-arm64` build is cross-compiled from `osx_64`
+elif [[ "${target_platform}" == osx-arm64 ]]; then
+    make HOSTCC="cc -g -Wall -pedantic -Wcast-qual" CC="$CC -g -Wall -pedantic -Wcast-qual"
 else
     make
 fi
